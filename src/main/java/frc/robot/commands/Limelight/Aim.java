33e2f5fbd30a458bb81193c4dc5fcbdc0fe39cc7
Chuckinator2020 <samjobuff@gmail.com>
2020-01-21 20:32:35 -0500

    01-21-2002 20:32:35

---
+++ src/main/java/frc/robot/commands/Limelight/Aim.java
@@ -0,0 +1,7 @@
+package frc.robot.commands.Limelight;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class Aim extends CommandBase {
+    
+}
---
+++ src/main/java/frc/robot/commands/Macros/AimAndShoot.java
@@ -0,0 +1,7 @@
+package frc.robot.commands.Macros;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class AimAndShoot extends SequentialCommandGroup {
+    
+}
