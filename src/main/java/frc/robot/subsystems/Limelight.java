922853b9eba93b3a54f622f32f14566cbb900aea
Chuckinator2020 <samjobuff@gmail.com>
2020-01-25 18:15:33 -0500

    cleanup

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -12,17 +11,11 @@ public class Limelight extends SubsystemBase {
     private Integer pipeline;
 
     private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    // NetworkTableEntry tx = table.getEntry("tx");
-    // NetworkTableEntry ty = table.getEntry("ty");
-    // NetworkTableEntry ta = table.getEntry("ta");
 
     public Limelight() {}
 
     @Override
     public void periodic() {
-        // this.x = tx.getDouble(0.0);
-        // this.y = ty.getDouble(0.0);
-        // this.area = ta.getDouble(0.0);
         this.x = table.getEntry("tx").getDouble(0.0);
         this.y = table.getEntry("ty").getDouble(0.0);
         this.area = table.getEntry("ta").getDouble(0.0);
