894642eabedebe50066ef798b53900a39f4cd6c5
Charles Buffington II <chuckinator2020@gmail.com>
2021-03-06 09:15:36 -0500

    adjust Shooter speed 1

--- src/main/java/frc/robot/commands/Macros/IndexAndShoot.java
+++ src/main/java/frc/robot/commands/Macros/IndexAndShoot.java
@@ -15,9 +15,11 @@ public class IndexAndShoot extends ParallelCommandGroup {
 
     // 24000.0
 
+    private static double SPEED = 10000.0;
+
     public IndexAndShoot(Intake intake, HorizIndexer horizIndexer, VertIndexer vertIndexer, Shooter shooter) {
         super(
-            new Shoot(shooter, 20000.0),
+            new Shoot(shooter, SPEED),
             new SequentialCommandGroup(
                 // new HorizIndexRevCycle(horizIndexer),
                 new WaitCommand(1.5),
@@ -28,7 +30,7 @@ public class IndexAndShoot extends ParallelCommandGroup {
 
     public IndexAndShoot(Intake intake, HorizIndexer horizIndexer, VertIndexer vertIndexer, Shooter shooter, Double waitTimeSeconds) {
         super(
-            new Shoot(shooter, 20000.0),
+            new Shoot(shooter, SPEED),
             new SequentialCommandGroup(
                 // new HorizIndexRevCycle(horizIndexer),
                 new WaitCommand(waitTimeSeconds),
